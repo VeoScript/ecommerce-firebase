6063d2ac98925f21e0c77908ab36a4eb825c9d17
acatzk <joshuaimalay@gmail.com>
2020-05-28 13:23:43 +0800

    added overview component

---
+++ src/views/Overview.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="overview">
+    <div class="container h-100 mt-5">
+      <div class="intro h-100">
+        <div class="row h-100 justify-content-center align-items-center">
+          <div class="col-md-6">
+            <h3>Overview Page</h3>
+          </div>
+          <div class="col-md-6">
+            <img src="@/assets/img/svg/overview.svg" alt="" class="img-fluid" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+
+export default {
+  name: 'Overview',
+}
+</script>
