9f731c5ca608d2b54da5d7ed7942c26e89ccfe4f
acatzk <joshuaimalay@gmail.com>
2020-05-28 11:26:18 +0800

    added form-login  component

---
+++ src/components/FormLogin.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h5 class="text-center">Login Please</h5>
+        <form @submit.prevent="login">
+            <div class="form-group">
+                <label>Email address</label>
+                <input type="email" v-model="email" class="form-control" placeholder="Enter email">
+            </div>
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" v-model="password" class="form-control" placeholder="Password">
+            </div>
+
+            <div class="form-group">
+                <button class="btn btn-primary" :disabled="email == ''">Login</button>
+            </div>
+        </form>
+    </div>
+</template>
