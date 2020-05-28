5493b91d73a00a108d4e4fae973da7b81eb48143
acatzk <joshuaimalay@gmail.com>
2020-05-28 11:11:02 +0800

    added route admin

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: () => import('@/views/Home.vue')
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    component: () => import('@/views/Admin.vue')
   }
 ];
 
