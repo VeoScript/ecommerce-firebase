efb77386c4d65fe34ebac7c41ec295e20c91b419
acatzk <joshuaimalay@gmail.com>
2020-05-28 13:35:23 +0800

    changed products design

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,7 +1,24 @@
 <template>
-  <div class="overview">
-    <div class="container">
-      <h3>Products Page</h3>
+  <div class="products">
+    <div class="container h-100 mt-5">
+      <div class="intro h-100">
+        <div class="row h-100 justify-content-center align-items-center">
+          <div class="col-md-6">
+            <h3>Products Page</h3>
+          </div>
+          <div class="col-md-6">
+            <img src="@/assets/img/svg/product.svg" alt="" class="img-fluid" />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+
+
+<script>
+
+export default {
+  name: 'Products',
+}
+</script>
