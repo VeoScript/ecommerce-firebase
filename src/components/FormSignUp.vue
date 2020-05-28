c61adefe8ccfd3686fcbe8c60aac1d4baaec771f
acatzk <joshuaimalay@gmail.com>
2020-05-28 11:29:46 +0800

    create form-sign-up component

---
+++ src/components/FormSignUp.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="tab-pane fade" id="pills-register" role="tabpanel" aria-labelledby="pills-register-tab">
+        <form @submit.prevent="signup">
+            <div class="form-group">
+                <label for="name">Your name</label>
+                <input type="text" v-model="name" class="form-control" placeholder="Your nice name">
+            </div>
+            <div class="form-group">
+                <label for="email">Email address</label>
+                <input type="email"  v-model="email" class="form-control" placeholder="Enter email">
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" v-model="password" class="form-control" placeholder="Password">
+            </div>
+            <div class="form-group">
+                <button class="btn btn-primary" @click="signup">Signup</button>
+            </div>
+        </form>
+    </div>
+</template>
