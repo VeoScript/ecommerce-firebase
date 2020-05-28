81150fd2eb1f1dd623f559f2010c5611fa487783
acatzk <joshuaimalay@gmail.com>
2020-05-28 13:36:38 +0800

    create orders component

---
+++ src/views/Orders.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="orders">
+    <div class="container h-100 mt-5">
+      <div class="intro h-100">
+        <div class="row h-100 justify-content-center align-items-center">
+          <div class="col-md-6">
+            <h3>Orders Page</h3>
+          </div>
+          <div class="col-md-6">
+            <img src="@/assets/img/svg/orders.svg" alt="" class="img-fluid" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+
+export default {
+  name: 'Orders',
+}
+</script>
