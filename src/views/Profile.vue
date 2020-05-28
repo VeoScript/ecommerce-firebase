5c1057a147f99a80114c78bd1d609a304f8e609b
acatzk <joshuaimalay@gmail.com>
2020-05-28 13:39:58 +0800

    create profile component

---
+++ src/views/Profile.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="profile">
+    <div class="container h-100 mt-5">
+      <div class="intro h-100">
+        <div class="row h-100 justify-content-center align-items-center">
+          <div class="col-md-6">
+            <h3>Profile Page</h3>
+          </div>
+          <div class="col-md-6">
+            <img src="@/assets/img/svg/profile.svg" alt="" class="img-fluid" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+
+export default {
+  name: 'Profile',
+}
+</script>
