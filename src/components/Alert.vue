28872206d84cbbcc26ef926c854a4ed80efbd634
acatzk <joshuaimalay@gmail.com>
2020-05-28 12:03:56 +0800

    added alert component

---
+++ src/components/Alert.vue
@@ -0,0 +1,20 @@
+<template>
+    <div :class="`alert alert-warning alert-dismissible fade show`" role="alert">
+        {{ message }}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Alert',
+    props: {
+        message: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
