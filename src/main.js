2210c0f2051ec5267018b7b01969173fc7fb19ea
acatzk <joshuaimalay@gmail.com>
2020-05-28 10:54:42 +0800

    import jquery bootstrap popper.js and app.scss

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "jquery";
+import "popper.js";
+import "bootstrap";
+import "@/assets/app.scss";
 
 Vue.config.productionTip = false;
 
